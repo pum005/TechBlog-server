35f012991cd35052e55a7de9fdba49eb143f44fe
pum005 <pum005@naver.com>
2023-08-17 12:56:20 +0900

    initial commit

---
+++ src/test/java/com/toy/techblog/TechblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.techblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
